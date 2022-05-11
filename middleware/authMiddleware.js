a8c8289c86cb221e81ccc0b5cb376b01670608ce
Priyank gondaliya <priyank.theappideas@gmail.com>
2022-05-11 18:04:40 +0530

    test

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,19 @@
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const createError = require('http-errors');
 const User = require('../models/usermodel');
 
 const checkUser = function (req, res, next) {
     const token = req.headers["authorization"] // || req.headers["x-access-token"];
     if (token) {
-        jwt.verify(token, process.env.SECRET_KEY, function(err, decodedToken){
+        jwt.verify(token, process.env.SECRET_KEY, function (err, decodedToken) {
             if (err) {
-                console.log("ERROR: "+err.message);
+                console.log("ERROR: " + err.message);
                 req.user = null;
                 next(createError.Unauthorized("Invalid token"));
             } else {
                 User.findById(decodedToken._id, function (err, user) {
                     if (err) {
-                        console.log("ERROR: "+err.message);
+                        console.log("ERROR: " + err.message);
                         req.user = null;
                         next(createError.InternalServerError("Some error occured!"));
                     }
