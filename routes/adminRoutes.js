ce50ee43294e13cb1d841af550076d6395069edf
Priyank Gondaliya <priyank.theappideas@gmail.com>
2022-06-15 11:01:52 +0530

    some

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -109,8 +109,12 @@ router.post("/changepass", checkAdmin, async (req, res) => {
         req.flash('green', 'Password updated.');
         return res.redirect('/admin/changepass');
     } catch (error) {
+        if (error) {
+            
         console.log(error);
         res.status(400).send(error.message);
+            console.log(error);
+        }
     }
 })
 
