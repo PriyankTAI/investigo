858981b2fd52d574c80cfde6bc5c1935c5caf116
Nikhil <nik.theappideas@gmail.com>
2022-06-30 17:43:10 +0530

    email validation in newsletter

--- models/newsletterModel.js
+++ models/newsletterModel.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const newsletterSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, "Please provide email."],
         unique: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Email is invalid")
+            }
+        }
     },
     date: {
         type: Date,
